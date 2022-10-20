2fbf0d732301113b78497d01159f1c5c1f0c8ad8
Sergio Uriza <syur2010@hotmail.com>
2022-10-20 01:06:03 -0500

    refactoring and modifying game logic

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,8 @@ const sectionMessage = document.getElementById("message")
 const listPlayerAttack = document.getElementById("list_player_attack")
 const listEnemyAttack = document.getElementById("list_enemy_attack")
 const listResult = document.getElementById("list_result")
-const spanLivesPlayer = document.getElementById("lives_player")
-const spanLivesEnemy = document.getElementById("lives_enemy")
+const spanVictoriesPlayer = document.getElementById("victories_player")
+const spanVictoriesEnemy = document.getElementById("victories_enemy")
 const imagePlayer = document.getElementById("image_player")
 const imageEnemy = document.getElementById("image_enemy")
 const paragraphCharacterEnemy = document.getElementById("character_enemy")
@@ -12,47 +12,110 @@ const paragraphCharacterPlayer = document.getElementById("character_player")
 const sectionSelectAttack = document.getElementById("select_attack")
 const sectionRestart = document.getElementById("restart")
 const buttonCharacterPlayer = document.getElementById("button_character")
-const rockButton = document.getElementById("button_rock")
-const paperButton = document.getElementById("button_paper")
-const scissorsButton = document.getElementById("button_scissors")
-const lizardButton = document.getElementById("button_lizard")
-const spockButton = document.getElementById("button_spock")
 const restartButton = document.getElementById("button_rest")
 const divCharacters = document.getElementById("div_characters")
+const divAttacks = document.getElementById("div_attacks")
+const divPlayer = document.getElementById("div_player")
+const divEnemy = document.getElementById("div_enemy")
 
 let characters = []
+let inputCharacters = []
+let selectedCharacterAttacks = []
+let selectedEnemyAttacks = []
+let selectedCharacterButtons
+let listPlayerAttacks = []
+let listEnemyAttacks = []
 let listOfCharacters
+let listOfAttack
 let playerAttack = ""
 let enemyAttack = ""
-let livesPlayer = 5
-let livesEnemy = 5
-let round = 1
+let playerWins = 0
+let enemyWins = 0
+let attacksLength = 0
+
+let allPossibleAttacks = [
+    { name: "Rock", id: "button_rock" },
+    { name: "Paper", id: "button_paper" },
+    { name: "Scissors", id: "button_scissors" },
+    { name: "Lizard", id: "button_lizard" },
+    { name: "Spock", id: "button_spock" }
+]
 
 class Character {
-    constructor(name, id, image, live) {
+    constructor(name, id, image, preference) {
         this.name = name
         this.id = id
         this.image = image
-        this.live = live
+        this.preference = preference
+        this.attacks = []
     }
 }
 
-let sheldon = new Character('Sheldon', 'character_one', './img/sheldon.png', 5)
-let leonard = new Character('Leonard', 'character_two', './img/leonard.png', 5)
-let howard = new Character('Howard', 'character_three', './img/howard.png', 5)
-let penny = new Character('Penny', 'character_four', './img/penny.png', 5)
-let bernadette = new Character('Bernadette', 'character_five', './img/bernadette.png', 5)
-let amy = new Character('Amy', 'character_six', './img/amy.png', 5)
+let sheldon = new Character("Sheldon", "character_one", "./img/sheldon.png", "spock")
+let leonard = new Character("Leonard", "character_two", "./img/leonard.png", "paper")
+let howard = new Character("Howard", "character_three", "./img/howard.png", "lizard")
+let penny = new Character("Penny", "character_four", "./img/penny.png", "scissors")
+let bernadette = new Character("Bernadette", "character_five", "./img/bernadette.png", "rock")
+let amy = new Character("Amy", "character_six", "./img/amy.png", "none")
 
-characters.push(sheldon, leonard, howard, penny, bernadette, amy)
+sheldon.attacks.push(
+    allPossibleAttacks[0],
+    allPossibleAttacks[2],
+    allPossibleAttacks[3],
+    allPossibleAttacks[4],
+    allPossibleAttacks[4],
+)
+
+leonard.attacks.push(
+    allPossibleAttacks[0],
+    allPossibleAttacks[1],
+    allPossibleAttacks[1],
+    allPossibleAttacks[2],
+    allPossibleAttacks[4],
+)
+
+howard.attacks.push(
+    allPossibleAttacks[0],
+    allPossibleAttacks[1],
+    allPossibleAttacks[3],
+    allPossibleAttacks[3],
+    allPossibleAttacks[4],
+)
+
+penny.attacks.push(
+    allPossibleAttacks[1],
+    allPossibleAttacks[2],
+    allPossibleAttacks[2],
+    allPossibleAttacks[3],
+    allPossibleAttacks[4],
+)
+
+bernadette.attacks.push(
+    allPossibleAttacks[0],
+    allPossibleAttacks[0],
+    allPossibleAttacks[1],
+    allPossibleAttacks[2],
+    allPossibleAttacks[3],
+)
+
+amy.attacks.push(
+    allPossibleAttacks[0],
+    allPossibleAttacks[1],
+    allPossibleAttacks[2],
+    allPossibleAttacks[3],
+    allPossibleAttacks[4],
+)
+
+characters.push(sheldon, leonard, howard, penny, bernadette, amy) // Agregar personaje en el arreglo
 characters.forEach((character) => {
     listOfCharacters = `
-    <input type="radio" name="characters" id=${character.id} />
-    <label for=${character.id}>
-        <p>${character.name}</p>
-        <img src=${character.image} alt=${character.name}>
-    </label>
-    `
+        <input type="radio" name="characters" id=${character.id} />
+        <label for=${character.id}>
+            <p>${character.name}</p>
+            <img src=${character.image} alt=${character.name}>
+            <p class="cpreference">Preference:<br>${character.preference}</p>
+        </label>
+        `
     divCharacters.innerHTML += listOfCharacters
 })
 
@@ -63,6 +126,8 @@ const inputCharacterFour = document.getElementById(characters[3].id)
 const inputCharacterFive = document.getElementById(characters[4].id)
 const inputCharacterSix = document.getElementById(characters[5].id)
 
+inputCharacters.push(inputCharacterOne, inputCharacterTwo, inputCharacterThree, inputCharacterFour, inputCharacterFive, inputCharacterSix) // Agregar input de los personajes en el arreglo
+
 function numRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -71,40 +136,30 @@ function restartGame() {
     location.reload()
 }
 
-function lastTransform(idWinner, idLoser) {
-    let winner = document.getElementById(idWinner)
+function lastTransform(winner, loser) {
     winner.style.backgroundColor = "#efb52154"
 
-    let loser = document.getElementById(idLoser)
     loser.style.transitionDuration = "0.8s"
     loser.style.transform = "scale(0.7)"
     loser.style.textDecoration = "line-through"
     loser.style.opacity = "0.7"
 }
 
-function disableBattleButtons() {
-    rockButton.disabled = true
-    paperButton.disabled = true
-    scissorsButton.disabled = true
-    lizardButton.disabled = true
-    spockButton.disabled = true
-    sectionRestart.style.display = "block" // Habilitar seccion de reinicio
-}
-
 function modifyMessage(result) {
     sectionMessage.innerHTML = result
 }
 
-function checkLives() {
-    if (livesPlayer === 0) {
-        modifyMessage("You're so bad, you LOSE the game")
-        disableBattleButtons()
-        lastTransform("div_enemy", "div_player")
-    } else if (livesEnemy === 0) {
+function checkWins() {
+    if (playerWins > enemyWins) {
         modifyMessage("¡Congratulations, you WIN the game!")
-        disableBattleButtons()
-        lastTransform("div_player", "div_enemy")
+        lastTransform(divPlayer, divEnemy)
+    } else if (playerWins < enemyWins) {
+        modifyMessage("You're so bad, you LOSE the game")
+        lastTransform(divEnemy, divPlayer)
+    } else if (playerWins === enemyWins) {
+        modifyMessage("How boring! tied game, nobody wins")
     }
+    sectionRestart.style.display = "block" // Habilitar seccion de reinicio
 }
 
 function createCombatMessage(result) {
@@ -117,99 +172,101 @@ function createCombatMessage(result) {
     let messageResult = document.createElement("p")
     messageResult.innerHTML = result
     listResult.appendChild(messageResult) // Listado de los resultados del duelo
+    spanVictoriesPlayer.innerHTML = playerWins // Conteo victorias jugador
+    spanVictoriesEnemy.innerHTML = enemyWins // Conteo victorias enemigo
 }
 
 function combat(player, opponent) {
     if (player === opponent) {
         createCombatMessage("-DRAW-")
-        modifyMessage(("Round " + round + ": Same attack, nothing happens"))
-        round++
     } else if ((player === "ROCK" && opponent === "SCISSORS") || (player === "ROCK" && opponent === "LIZARD") || (player === "PAPER" && opponent === "ROCK") || (player === "PAPER" && opponent === "SPOCK") || (player === "SCISSORS" && opponent === "PAPER") || (player === "SCISSORS" && opponent === "LIZARD") || (player === "LIZARD" && opponent === "PAPER") || (player === "LIZARD" && opponent === "SPOCK") || (player === "SPOCK" && opponent === "ROCK") || (player === "SPOCK" && opponent === "SCISSORS")) {
+        playerWins++
         createCombatMessage("-WIN-")
-        modifyMessage(("Round " + round + ": You kill him, well done"))
-        round++
-        livesEnemy--
-        spanLivesEnemy.innerHTML = livesEnemy
     } else if ((opponent === "ROCK" && player === "SCISSORS") || (opponent === "ROCK" && player === "LIZARD") || (opponent === "PAPER" && player === "ROCK") || (opponent === "PAPER" && player === "SPOCK") || (opponent === "SCISSORS" && player === "PAPER") || (opponent === "SCISSORS" && player === "LIZARD") || (opponent === "LIZARD" && player === "PAPER") || (opponent === "LIZARD" && player === "SPOCK") || (opponent === "SPOCK" && player === "ROCK") || (opponent === "SPOCK" && player === "SCISSORS")) {
+        enemyWins++
         createCombatMessage("-LOSE-")
-        modifyMessage(("Round " + round + ": You die, very sad"))
-        round++
-        livesPlayer--
-        spanLivesPlayer.innerHTML = livesPlayer
     } else {
         createCombatMessage("ANY PROBLEM")
     }
-    checkLives()
+}
+
+function combatSequence() {
+    for (let i = 0; i < listPlayerAttacks.length; i++) {
+        playerAttack = listPlayerAttacks[i]
+        enemyAttack = listEnemyAttacks[i]
+        combat(listPlayerAttacks[i], listEnemyAttacks[i])
+    }
+    checkWins()
 }
 
 function enemyAttackRandom() {
-    let attackRandom = numRandom(1, 5)
+    selectedEnemyAttacks.forEach(attack => {
+        listEnemyAttacks.push(attack.name.toUpperCase())
+    });
+    listEnemyAttacks.sort(() => Math.random() - 0.5) // Desordenar aleatoriamente el orden de los ataques del enemigo //array = array.sort(function() {return Math.random() - 0.5})
 
-    if (attackRandom === 1) {
-        enemyAttack = "ROCK"
-    } else if (attackRandom === 2) {
-        enemyAttack = "PAPER"
-    } else if (attackRandom === 3) {
-        enemyAttack = "SCISSORS"
-    } else if (attackRandom === 4) {
-        enemyAttack = "LIZARD"
-    } else {
-        enemyAttack = "SPOCK"
-    }
-    combat(playerAttack, enemyAttack)
+    combatSequence()
 }
 
-function rockAttack() {
-    playerAttack = "ROCK"
-    enemyAttackRandom()
-}
-
-function paperAttack() {
-    playerAttack = "PAPER"
-    enemyAttackRandom()
-}
-
-function scissorsAttack() {
-    playerAttack = "SCISSORS"
-    enemyAttackRandom()
-}
-
-function lizardAttack() {
-    playerAttack = "LIZARD"
-    enemyAttackRandom()
-}
-
-function spockAttack() {
-    playerAttack = "SPOCK"
-    enemyAttackRandom()
-}
-
-function modifyDuelImages(character, result) {
-    if (character === characters[0].name.toUpperCase()) {
-        result.src = characters[0].image
-    } else if (character === characters[1].name.toUpperCase()) {
-        result.src = characters[1].image
-    } else if (character === characters[2].name.toUpperCase()) {
-        result.src = characters[2].image
-    } else if (character === characters[3].name.toUpperCase()) {
-        result.src = characters[3].image
-    } else if (character === characters[4].name.toUpperCase()) {
-        result.src = characters[4].image
-    } else if (character === characters[5].name.toUpperCase()) {
-        result.src = characters[5].image
+function startCombat() {
+    if (listPlayerAttacks.length === attacksLength) {
+        enemyAttackRandom()
     }
 }
 
-function insertDuelImages() {
-    modifyDuelImages(paragraphCharacterPlayer.innerHTML, imagePlayer) // Editar imagen personaje jugador
-    modifyDuelImages(paragraphCharacterEnemy.innerHTML, imageEnemy) // Editar imagen personaje enemigo
+function sequenceAttacks() {
+    selectedCharacterButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            allPossibleAttacks.forEach(attack => {
+                if (event.target.textContent === attack.name) {
+                    listPlayerAttacks.push(attack.name.toUpperCase())
+                    button.disabled = true
+                    button.style.backgroundColor = "gray"
+                }
+            });
+            startCombat()
+        })
+    })
+}
+
+function insertAttacks() {
+    selectedCharacterAttacks.forEach((attack) => {
+        listOfAttack = `
+            <button class="cbutton-attack" id=${attack.id}>${attack.name}</button>
+            `
+        divAttacks.innerHTML += listOfAttack
+    })
+    selectedCharacterButtons = document.querySelectorAll(".cbutton-attack")
+    sequenceAttacks()
+}
+
+function extractAttacks() {
+    characters.forEach(character => {
+        if (paragraphCharacterPlayer.innerHTML.toLowerCase() === character.name.toLowerCase()) {
+            selectedCharacterAttacks = character.attacks
+        }
+        if (paragraphCharacterEnemy.innerHTML.toLowerCase() === character.name.toLowerCase()) {
+            selectedEnemyAttacks = character.attacks
+        }
+    });
+    insertAttacks()
+}
+
+function modifyDuelImages(characterName, editSource) {
+    characters.forEach(character => {
+        if (characterName === character.name.toUpperCase()) {
+            editSource.src = character.image
+        }
+    });
 }
 
 function selectCharacterEnemy() {
     let inputCharacterEnemy = numRandom(0, characters.length - 1)
-
     paragraphCharacterEnemy.innerHTML = characters[inputCharacterEnemy].name.toUpperCase()
-    insertDuelImages()
+
+    modifyDuelImages(paragraphCharacterPlayer.innerHTML, imagePlayer) // Editar imagen personaje jugador
+    modifyDuelImages(paragraphCharacterEnemy.innerHTML, imageEnemy) // Editar imagen personaje enemigo
+    extractAttacks()
 }
 
 function transition() {
@@ -218,32 +275,23 @@ function transition() {
 }
 
 function selectCharacterPlayer() {
-    if (inputCharacterOne.checked) {
-        paragraphCharacterPlayer.innerHTML = characters[0].name.toUpperCase()
-    } else if (inputCharacterTwo.checked) {
-        paragraphCharacterPlayer.innerHTML = characters[1].name.toUpperCase()
-    } else if (inputCharacterThree.checked) {
-        paragraphCharacterPlayer.innerHTML = characters[2].name.toUpperCase()
-    } else if (inputCharacterFour.checked) {
-        paragraphCharacterPlayer.innerHTML = characters[3].name.toUpperCase()
-    } else if (inputCharacterFive.checked) {
-        paragraphCharacterPlayer.innerHTML = characters[4].name.toUpperCase()
-    } else if (inputCharacterSix.checked) {
-        paragraphCharacterPlayer.innerHTML = characters[5].name.toUpperCase()        
-    } else {
+    let condition = false
+    for (let i = 0; i < inputCharacters.length; i++) {
+        if (inputCharacters[i].checked) {
+            paragraphCharacterPlayer.innerHTML = characters[i].name.toUpperCase()
+            attacksLength = characters[i].attacks.length
+            condition = true
+        }
+    }
+    if (!condition) {
         alert("You have to select a character")
         return
     }
-    transition()
     selectCharacterEnemy()
+    transition()
 }
 
 sectionSelectAttack.style.display = "none" // Deshabilitar seccion de ataque
 sectionRestart.style.display = "none" // Deshabilitar seccion de reinicio
 buttonCharacterPlayer.addEventListener("click", selectCharacterPlayer) // Accion boton de seleccion
-rockButton.addEventListener("click", rockAttack) // Accion botones de Batalla
-paperButton.addEventListener("click", paperAttack) // Accion botones de Batalla
-scissorsButton.addEventListener("click", scissorsAttack) // Accion botones de Batalla
-lizardButton.addEventListener("click", lizardAttack) // Accion botones de Batalla
-spockButton.addEventListener("click", spockAttack) // Accion botones de Batalla
 restartButton.addEventListener("click", restartGame) // Accion boton de reinicio
